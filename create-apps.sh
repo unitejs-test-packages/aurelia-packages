741edad13b6f9852d921c3f69f58d7a880fd9105
Martyn Janes <martyn@obany.com>
2017-12-15 16:12:28 +0300

    Updated rxjs use

--- create-apps.sh
+++ create-apps.sh
@@ -6,7 +6,7 @@ mkdir -p ./apps/aurelia-javascript/www/test/e2e/src/examples/ && cp -r ./unitejs
 mkdir -p ./apps/aurelia-javascript/www/test/e2e/src/helpers/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/helpers/*.js $_
 node unitejs/unitejs-cli/bin/unite package --packageName=lodash --outputDirectory=./apps/aurelia-javascript
 node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/aurelia-javascript
-# node unitejs/unitejs-cli/bin/unite package --packageName=rxjs --outputDirectory=./apps/aurelia-javascript
+node unitejs/unitejs-cli/bin/unite package --packageName=rxjs --outputDirectory=./apps/aurelia-javascript
 node unitejs/unitejs-cli/bin/unite package --packageName=underscore --outputDirectory=./apps/aurelia-javascript
 
 node unitejs/unitejs-cli/bin/unite configure --packageName=aurelia-typescript-amd --title="Aurelia TypeScript AMD" --profile=AureliaTypeScript --moduleType=AMD --bundler=RequireJS --outputDirectory=./apps/aurelia-typescript-amd
@@ -22,5 +22,5 @@ mkdir -p ./apps/aurelia-typescript-systemjs/www/test/e2e/src/examples/ && cp -r 
 mkdir -p ./apps/aurelia-typescript-systemjs/www/test/e2e/src/helpers/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/helpers/*.ts $_
 node unitejs/unitejs-cli/bin/unite package --packageName=lodash --outputDirectory=./apps/aurelia-typescript-systemjs
 node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/aurelia-typescript-systemjs
-# node unitejs/unitejs-cli/bin/unite package --packageName=rxjs --outputDirectory=./apps/aurelia-typescript-systemjs
+node unitejs/unitejs-cli/bin/unite package --packageName=rxjs --outputDirectory=./apps/aurelia-typescript-systemjs
 node unitejs/unitejs-cli/bin/unite package --packageName=underscore --outputDirectory=./apps/aurelia-typescript-systemjs
