260c62d6aed53ba77d34ebe699d8f4e58a0d9d81
Martyn Janes <martyn@obany.com>
2017-12-15 11:19:38 +0300

    Added underscore

--- create-apps.sh
+++ create-apps.sh
@@ -6,15 +6,18 @@ mkdir -p ./apps/aurelia-javascript/www/test/e2e/src/examples/ && cp -r ./unitejs
 mkdir -p ./apps/aurelia-javascript/www/test/e2e/src/helpers/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/helpers/*.js $_
 node unitejs/unitejs-cli/bin/unite package --packageName=lodash --outputDirectory=./apps/aurelia-javascript
 node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/aurelia-javascript
+node unitejs/unitejs-cli/bin/unite package --packageName=underscore --outputDirectory=./apps/aurelia-javascript
 
 node unitejs/unitejs-cli/bin/unite configure --packageName=aurelia-typescript-amd --title="Aurelia TypeScript AMD" --profile=AureliaTypeScript --moduleType=AMD --bundler=RequireJS --outputDirectory=./apps/aurelia-typescript-amd
 mkdir -p ./apps/aurelia-typescript-amd/www/test/e2e/src/examples/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/examples/*.ts $_
 mkdir -p ./apps/aurelia-typescript-amd/www/test/e2e/src/helpers/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/helpers/*.ts $_
 node unitejs/unitejs-cli/bin/unite package --packageName=lodash --outputDirectory=./apps/aurelia-typescript-amd
 node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/aurelia-typescript-amd
+node unitejs/unitejs-cli/bin/unite package --packageName=underscore --outputDirectory=./apps/aurelia-typescript-amd
 
 node unitejs/unitejs-cli/bin/unite configure --packageName=aurelia-typescript-systemjs --title="Aurelia TypeScript SystemJS" --profile=AureliaTypeScript --moduleType=SystemJS --bundler=SystemJSBuilder --outputDirectory=./apps/aurelia-typescript-systemjs
 mkdir -p ./apps/aurelia-typescript-systemjs/www/test/e2e/src/examples/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/examples/*.ts $_
 mkdir -p ./apps/aurelia-typescript-systemjs/www/test/e2e/src/helpers/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/helpers/*.ts $_
 node unitejs/unitejs-cli/bin/unite package --packageName=lodash --outputDirectory=./apps/aurelia-typescript-systemjs
 node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/aurelia-typescript-systemjs
+node unitejs/unitejs-cli/bin/unite package --packageName=underscore --outputDirectory=./apps/aurelia-typescript-systemjs
