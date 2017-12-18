86da95bd833f9bff7f124350339d4822b23e8403
Martyn Janes <martyn@obany.com>
2017-12-18 10:27:31 +0300

    Added JavaScript SystemJS Builds

--- create-apps.sh
+++ create-apps.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 set -e
 
-node unitejs/unitejs-cli/bin/unite configure --packageName=aurelia-javascript --title="Aurelia JavaScript" --profile=AureliaJavaScript --outputDirectory=./apps/aurelia-javascript
-mkdir -p ./apps/aurelia-javascript/www/test/e2e/src/examples/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/examples/*.js $_
-mkdir -p ./apps/aurelia-javascript/www/test/e2e/src/helpers/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/helpers/*.js $_
-node unitejs/unitejs-cli/bin/unite package --packageName=lodash --outputDirectory=./apps/aurelia-javascript
-node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/aurelia-javascript
-node unitejs/unitejs-cli/bin/unite package --packageName=rxjs --outputDirectory=./apps/aurelia-javascript
-node unitejs/unitejs-cli/bin/unite package --packageName=underscore --outputDirectory=./apps/aurelia-javascript
+node unitejs/unitejs-cli/bin/unite configure --packageName=aurelia-javascript-amd --title="Aurelia JavaScript CommonJS" --profile=AureliaJavaScript --moduleType=AMD --bundler=RequireJS --outputDirectory=./apps/aurelia-javascript-amd
+mkdir -p ./apps/aurelia-javascript-amd/www/test/e2e/src/examples/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/examples/*.js $_
+mkdir -p ./apps/aurelia-javascript-amd/www/test/e2e/src/helpers/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/helpers/*.js $_
+node unitejs/unitejs-cli/bin/unite package --packageName=lodash --outputDirectory=./apps/aurelia-javascript-amd
+node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/aurelia-javascript-amd
+# node unitejs/unitejs-cli/bin/unite package --packageName=rxjs --outputDirectory=./apps/aurelia-javascript-amd
+node unitejs/unitejs-cli/bin/unite package --packageName=underscore --outputDirectory=./apps/aurelia-javascript-amd
+
+node unitejs/unitejs-cli/bin/unite configure --packageName=aurelia-javascript --title="Aurelia JavaScript SystemJS" --profile=AureliaJavaScript --moduleType=SystemJS --bundler=SystemJSBuilder --outputDirectory=./apps/aurelia-javascript-systemjs
+mkdir -p ./apps/aurelia-javascript-systemjs/www/test/e2e/src/examples/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/examples/*.js $_
+mkdir -p ./apps/aurelia-javascript-systemjs/www/test/e2e/src/helpers/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/helpers/*.js $_
+node unitejs/unitejs-cli/bin/unite package --packageName=lodash --outputDirectory=./apps/aurelia-javascript-systemjs
+node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/aurelia-javascript-systemjs
+node unitejs/unitejs-cli/bin/unite package --packageName=rxjs --outputDirectory=./apps/aurelia-javascript-systemjs
+node unitejs/unitejs-cli/bin/unite package --packageName=underscore --outputDirectory=./apps/aurelia-javascript-systemjs
 
 node unitejs/unitejs-cli/bin/unite configure --packageName=aurelia-typescript-amd --title="Aurelia TypeScript AMD" --profile=AureliaTypeScript --moduleType=AMD --bundler=RequireJS --outputDirectory=./apps/aurelia-typescript-amd
 mkdir -p ./apps/aurelia-typescript-amd/www/test/e2e/src/examples/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/examples/*.ts $_
