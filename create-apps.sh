6dfbf9098431eed9b6b7902455d92700166ae957
Martyn Janes <martyn@obany.com>
2017-12-18 10:37:14 +0300

    Added JavaScript SystemJS Builds

--- create-apps.sh
+++ create-apps.sh
@@ -4,6 +4,7 @@ set -e
 node unitejs/unitejs-cli/bin/unite configure --packageName=aurelia-javascript-amd --title="Aurelia JavaScript CommonJS" --profile=AureliaJavaScript --moduleType=AMD --bundler=RequireJS --outputDirectory=./apps/aurelia-javascript-amd
 mkdir -p ./apps/aurelia-javascript-amd/www/test/e2e/src/examples/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/examples/*.js $_
 mkdir -p ./apps/aurelia-javascript-amd/www/test/e2e/src/helpers/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/helpers/*.js $_
+rm ./apps/aurelia-typescript-amd/www/test/e2e/src/examples/example-rxjs.spec.ts
 node unitejs/unitejs-cli/bin/unite package --packageName=lodash --outputDirectory=./apps/aurelia-javascript-amd
 node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/aurelia-javascript-amd
 # node unitejs/unitejs-cli/bin/unite package --packageName=rxjs --outputDirectory=./apps/aurelia-javascript-amd
